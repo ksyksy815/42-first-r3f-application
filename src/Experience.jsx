ded403867f6497c8c39725f783b638d261317e80
Seoyeon Kim <seoyeonkim815@gmail.com>
2022-12-11 22:08:31 +0900

    [R3F] meshs + geometry and material props

---
+++ src/Experience.jsx
@@ -0,0 +1,89 @@
+import helper from './helper'
+
+export default function Experience() {
+
+
+  /* Hooks */
+
+
+  /* Helpers */
+
+  
+  /* Render */
+
+  const renderSphere = () => {
+    const meshParams = {
+      position: [-1.5, -0.5, 0],
+      scale: 2
+    }
+
+    const geoParams = {
+      args: [0.5, 20, 20]
+    }
+
+    const materialArgs = {
+      args: [{
+        color: "pink",
+        wireframe: true,
+      }]
+    } 
+
+    return (
+      <mesh {...helper.generateMeshProps({...meshParams})} >
+        <sphereGeometry {...helper.generateGeoProps({...geoParams})} />
+        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})}  />
+      </mesh>
+    )
+  }
+
+  const renderBox = () => {
+    const meshParams = {
+      position: [2, -1, 0],
+      rotation: [2, 0, 2],
+    }
+
+    const materialArgs = {
+      args: [{
+        color: "mediumpurple",
+        wireframe: true,
+      }]
+    } 
+    
+    return (
+      <mesh {...helper.generateMeshProps({...meshParams})} >
+        <boxGeometry {...helper.generateGeoProps()} />
+        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})}  />
+      </mesh>
+    )
+  }
+
+  const renderPlane = () => {
+    const meshParams = {
+      position: [0, -2, 0],
+      rotation: [-1.8, 0, 0],
+      scale: 4,
+    }
+
+    const materialArgs = {
+      args: [{
+        color: "green"
+      }]
+    } 
+
+    return (
+      <mesh {...helper.generateMeshProps({...meshParams})} >
+        <planeGeometry {...helper.generateGeoProps()} />
+        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})} />
+      </mesh>
+    )
+  }
+
+  return (
+    <>
+      {renderSphere()}
+      {renderBox()}
+      {renderPlane()}
+    </>
+  )
+
+}
