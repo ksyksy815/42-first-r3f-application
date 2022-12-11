908f47261981bd222c61796e87b93172507532ee
Seoyeon Kim <seoyeonkim815@gmail.com>
2022-12-11 22:08:14 +0900

    [R3F] meshs + geometry and material props

---
+++ src/helper.js
@@ -0,0 +1,45 @@
+class Helper {
+
+  generateMeshProps = (meshParams) => {
+    const defaultValues = {
+      position: [0, 0, 0],
+      scale: 1,
+      rotation: [0, 0, 0],
+    }
+
+    return {
+      position: meshParams?.position || defaultValues.position,
+      scale: meshParams?.scale || defaultValues.scale,
+      rotation: meshParams?.rotation || defaultValues.rotation,
+    }
+  }
+
+  generateGeoProps = (geoProps) => {
+    const defaultValues = {
+      args: [1, 1, 1],
+    }
+
+    return {
+      args: geoProps?.args || defaultValues.args,
+    }
+  }
+
+  generateMaterialProps = (materialProps) => {
+    const defaultValues = {
+      args: [ {
+        color: "yellow",
+      } ]
+    }
+
+    const argsValue = materialProps?.args || [...defaultValues.args]
+
+    return {
+      args: argsValue,
+    }
+  }
+
+}
+
+const helper = new Helper()
+
+export default helper;
