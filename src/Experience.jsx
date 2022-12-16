e6be2e555cf820ece31e3621e54e9d1876eaa747
Seoyeon Kim <seoyeonkim815@gmail.com>
2022-12-16 11:12:48 +0900

    [R3F] Applied Prettier settings

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,58 +1,54 @@
-import helper from './helper'
+import helper from "./helper";
 
 export default function Experience() {
-
-
   /* Hooks */
-
 
   /* Helpers */
 
-  const getMeshParams = (position, rotation, scale, ...restParams) => ({ position, rotation, scale, ...restParams })
-
-  const getGeometryArgs = (radius, width, height) => ({ args: [radius, width, height] })
-
-  const getMaterialArgs = (color, wireframe) => ({ args: [{color, wireframe: wireframe || false}] })
-
-  
   /* Render */
 
   const renderSphere = () => {
-    const meshParams = getMeshParams([-1.5, -0.5, 0], null, 2) 
-    const geoParams = getGeometryArgs(0.5, 20, 20)
-    const materialArgs = getMaterialArgs('pink', true)
+    const meshParams = helper.getMeshParams([-1.5, -0.5, 0], null, 2);
+    const geoParams = helper.getGeometryArgs(0.5, 20, 20);
+    const materialArgs = helper.getMaterialArgs("pink", true);
 
     return (
-      <mesh {...helper.generateMeshProps({...meshParams})} >
-        <sphereGeometry {...helper.generateGeoProps({...geoParams})} />
-        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})}  />
+      <mesh {...helper.generateMeshProps({ ...meshParams })}>
+        <sphereGeometry {...helper.generateGeoProps({ ...geoParams })} />
+        <meshBasicMaterial
+          {...helper.generateMaterialProps({ ...materialArgs })}
+        />
       </mesh>
-    )
-  }
+    );
+  };
 
   const renderBox = () => {
-    const meshParams = getMeshParams([2, -1, 0], [2, 0, 2])
-    const materialArgs = getMaterialArgs('mediumpurple', true)
-    
+    const meshParams = helper.getMeshParams([2, -1, 0], [2, 0, 2]);
+    const materialArgs = helper.getMaterialArgs("mediumpurple", true);
+
     return (
-      <mesh {...helper.generateMeshProps({...meshParams})} >
+      <mesh {...helper.generateMeshProps({ ...meshParams })}>
         <boxGeometry {...helper.generateGeoProps()} />
-        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})}  />
+        <meshBasicMaterial
+          {...helper.generateMaterialProps({ ...materialArgs })}
+        />
       </mesh>
-    )
-  }
+    );
+  };
 
   const renderPlane = () => {
-    const meshParams = getMeshParams([0, -2, 0], [-1.8, 0, 0], 4)
-    const materialArgs = getMaterialArgs('green')
+    const meshParams = helper.getMeshParams([0, -2, 0], [-1.8, 0, 0], 4);
+    const materialArgs = helper.getMaterialArgs("green");
 
     return (
-      <mesh {...helper.generateMeshProps({...meshParams})} >
+      <mesh {...helper.generateMeshProps({ ...meshParams })}>
         <planeGeometry {...helper.generateGeoProps()} />
-        <meshBasicMaterial {...helper.generateMaterialProps({...materialArgs})} />
+        <meshBasicMaterial
+          {...helper.generateMaterialProps({ ...materialArgs })}
+        />
       </mesh>
-    )
-  }
+    );
+  };
 
   return (
     <>
@@ -60,6 +56,5 @@ export default function Experience() {
       {renderBox()}
       {renderPlane()}
     </>
-  )
-
+  );
 }
--- src/helper.js
+++ src/helper.js
@@ -1,45 +1,60 @@
 class Helper {
+  getMeshParams = (position, rotation, scale, ...restParams) => ({
+    position,
+    rotation,
+    scale,
+    ...restParams,
+  });
+
+  getGeometryArgs = (radius, width, height) => ({
+    args: [radius, width, height],
+  });
+
+  getMaterialArgs = (color, wireframe) => ({
+    args: [{ color, wireframe: wireframe || false }],
+  });
 
   generateMeshProps = (meshParams) => {
     const defaultValues = {
       position: [0, 0, 0],
       scale: 1,
       rotation: [0, 0, 0],
-    }
+    };
 
     return {
       position: meshParams?.position || defaultValues.position,
       scale: meshParams?.scale || defaultValues.scale,
       rotation: meshParams?.rotation || defaultValues.rotation,
-    }
-  }
+    };
+  };
 
   generateGeoProps = (geoProps) => {
     const defaultValues = {
       args: [1, 1, 1],
-    }
+    };
 
     return {
       args: geoProps?.args || defaultValues.args,
-    }
-  }
+    };
+  };
 
   generateMaterialProps = (materialProps) => {
     const defaultValues = {
-      args: [ {
-        color: "yellow",
-      } ]
-    }
+      args: [
+        {
+          color: "yellow",
+        },
+      ],
+    };
 
-    const argsValue = materialProps?.args || [...defaultValues.args]
+    const argsValue = materialProps?.args || [...defaultValues.args];
 
     return {
       args: argsValue,
-    }
-  }
-
+    };
+  };
 }
 
-const helper = new Helper()
+const helper = new Helper();
 
 export default helper;
